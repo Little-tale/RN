197f757cd49e02545c53d0ecd4fd361dde92f11f
Kim JaeHyung <you3192001@icloud.com>
2025-12-03 18:46:51 +0900

    [Feat] 만든것들 종합해 활용해보기

--- react_native_01/App.tsx
+++ react_native_01/App.tsx
@@ -6,9 +6,13 @@
  */
 
 import { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, Button } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+// MARK: subView Import
 import Header from './src/header';
+import Generator from './src/generator';
+import NumList from './src/numList';
+import Input from './src/input';
 
 /*
 style={{ // 인라인 스타일을 쓰면 경고가 나옴 -> 비추한다함
@@ -18,9 +22,63 @@ style={{ // 인라인 스타일을 쓰면 경고가 나옴 -> 비추한다함
           }}
 */
 function App() {
-  const [state] = useState({
+  type State = {
+    appName: string;
+    randomNumbers: number[];
+    textInput: string;
+  };
+
+  const [state, setState] = useState<State>({
     appName: 'My First React Native App',
+    randomNumbers: [],
+    textInput: '',
   });
+
+  const randomNumberSet = () => {
+    return Math.floor(Math.random() * 100);
+  };
+
+  const tapAddNumber = () => {
+    const number = randomNumberSet();
+    setState(prev => ({
+      ...prev,
+      randomNumbers: [...prev.randomNumbers, number],
+    }));
+  };
+
+  const tapDeleteNumber = (item: number, idx: number) => {
+    const newNumbers = state.randomNumbers.filter((num, index) => {
+      return idx !== index;
+    });
+    setState(prev => ({
+      ...prev,
+      randomNumbers: newNumbers,
+    }));
+  };
+
+  const onChangeTextInput = (text: string) => {
+    setState(prev => ({
+      ...prev,
+      textInput: text,
+    }));
+  };
+
+  const resetTextInput = () => {
+    setState(prev => ({
+      ...prev,
+      textInput: '',
+    }));
+  };
+
+  const addTextNumber = (text: String) => {
+    const toNumber = Number(text);
+    if (Number.isNaN(toNumber)) return;
+
+    setState(prev => ({
+      ...prev,
+      randomNumbers: [...prev.randomNumbers, toNumber],
+    }));
+  };
 
   return (
     <SafeAreaProvider>
@@ -35,6 +93,32 @@ function App() {
           <Text style={appStyles.mainText}>Hello World</Text>
         </View> */}
         <Header name={state.appName} />
+        <Input
+          textInput={state.textInput}
+          onChangeTextInput={onChangeTextInput}
+        />
+        <Button
+          title="Add Number"
+          onPress={() => {
+            addTextNumber(state.textInput);
+            resetTextInput();
+          }}
+        />
+        <Generator onPress={tapAddNumber} />
+        <ScrollView
+          style={appStyles.scrollView}
+          // onMomentumScrollBegin={}
+          // onMomentumScrollEnd={}
+          // onScroll={}
+          // onContentSizeChange={(width, height)}
+          // bounces={false}
+        >
+          <NumList
+            randomNumbers={state.randomNumbers}
+            onPressItem={tapDeleteNumber}
+          />
+        </ScrollView>
+        {/* <Text>SSSSS</Text> */}
       </SafeAreaView>
     </SafeAreaProvider>
   );
@@ -43,9 +127,9 @@ function App() {
 const appStyles = StyleSheet.create({
   mainView: {
     flex: 1,
-    backgroundColor: '#ffffff',
+    backgroundColor: '#a8a8a8',
     alignItems: 'center',
-    justifyContent: 'center',
+    // justifyContent: 'center',
   },
   subView: {
     // flex: 1,
@@ -65,6 +149,9 @@ const appStyles = StyleSheet.create({
     fontWeight: 'bold',
     color: '#d00000',
     padding: 10,
+  },
+  scrollView: {
+    width: '100%',
   },
 });
 
